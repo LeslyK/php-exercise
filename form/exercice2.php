6693fe23a31d94f0b5de730f040d44c575ba84e5
LeslyK <kobadobo@gmail.com>
2017-03-22 14:24:54 +0100

    method post

--- form/exercice2.php
+++ form/exercice2.php
@@ -4,18 +4,12 @@
 	<title>page php</title>
 </head>
 <body>
-	<form action="user.pho">
+	<form action="user.php" method="POST">
 		<label for="name">Nom</label>
 		<input type="text" name="name">
 		<label for="firstName">Prenom</label>
 		<input type="text" name="firstName">
-		<input type="button" name="submit" value="envouer">
+		<input type="submit" value="envouer">
 	</form>
-
-	<?php
-		echo "Hello";
-		
-	?>
-
 </body>
 </html>
